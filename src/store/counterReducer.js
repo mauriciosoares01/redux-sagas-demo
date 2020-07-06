6e49651197a8be121d0da49135f6dd17a3e2d351
mauriciosoares01 <mss.soares@hotmail.com>
2020-07-06 17:47:29 -0300

    start counter reducer

--- src/store/counterReducer.js
+++ src/store/counterReducer.js
@@ -1,1 +1,17 @@
+const INCREASE_COUNTER_ASYNC = 'INCREASE_COUNTER_ASYNC';
 
+const initialState = {
+  counter: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREASE_COUNTER_ASYNC:
+      return {
+        ...state,
+        counter: state.counter + action.value,
+      };
+    default:
+      break;
+  }
+};
